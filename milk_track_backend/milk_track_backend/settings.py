4390986b7c05801dc925d391e57f0dfab2c5329f
wondmD <wondmenehdereje@gmail.com>
2026-08-14 15:57:41 +0300

    db

--- milk_track_backend/milk_track_backend/settings.py
+++ milk_track_backend/milk_track_backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -98,14 +99,20 @@ WSGI_APPLICATION = 'milk_track_backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'milk-track-dereje'),
+        'USER': os.getenv('DB_USER', 'wondmeneh'),
+        'PASSWORD': os.getenv('DB_PASSWORD', '7817wonaD@db'),
+        'HOST': os.getenv('DB_HOST', '127.0.0.200'),
+        'PORT': os.getenv('DB_PORT', '5432'),
+        'CONN_MAX_AGE': int(os.getenv('DB_CONN_MAX_AGE', '60')),
+        'OPTIONS': {
+            'connect_timeout': 10,
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.1/ref/settings/#auth-password-validators
